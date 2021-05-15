27ebbf3d9bd70ad3fbb28c4f18493e05a94f7b3c
dmaslov <dmaslov@neoflex.ru>
2021-05-16 00:06:58 +0300

    Добавляет дополнительные условия отображения фильмов

--- src/main.js
+++ src/main.js
@@ -8,19 +8,16 @@ import {createFilmsListTemplate} from './view/films-list';
 import {createFooterTemplate} from './view/footer';
 import {generateFilm} from './mock/film';
 import {createNoFilmsListTemplate} from './view/no-films-list';
-import {
-  countFavorites,
-  countHistory,
-  countMoviesInside,
-  countViewedFilms,
-  countWatchlist
-} from './mock/other';
 
-const FILMS_COUNT = 6;
-const INIT_FILMS_COUNT = 5;
+const FILMS_COUNT = 8;
+const films = new Array(FILMS_COUNT).fill().map((item, i) => generateFilm(i)); // Генерация массива фильмов
+const INIT_FILMS_COUNT = Math.min(5, films.length);
 const SHOW_MORE_COUNT = 5;
 
-const films = new Array(FILMS_COUNT).fill().map((item, i) => generateFilm(i)); // Генерация массива фильмов
+const countWatchlist = films.filter((film) => film.user_details.watchlist).length;
+const countFavorites = films.filter((film) => film.user_details.favorite).length;
+const countHistory = films.filter((film) => film.user_details.already_watched).length;
+const countMoviesInside = films.length;
 
 const render = (container, template, place) => { // Функция render
   container.insertAdjacentHTML(place, template);
@@ -29,7 +26,7 @@ const render = (container, template, place) => { // Функция render
 const siteHeaderElement = document.querySelector('.header');
 const siteMainElement = document.querySelector('.main');
 
-render(siteHeaderElement, createHeaderProfileTemplate(countViewedFilms), 'beforeend'); // Создание названия пользователя в шапке
+render(siteHeaderElement, createHeaderProfileTemplate(countHistory), 'beforeend'); // Создание названия пользователя в шапке
 render(siteMainElement, createFilterTemplate(countWatchlist, countHistory, countFavorites), 'afterbegin');
 render(siteMainElement, createSortTemplate(FILMS_COUNT), 'beforeend');
 render(siteMainElement, createFilmsTemplate(), 'beforeend'); // Создание блока с фильмами
@@ -39,7 +36,7 @@ const filmsMainElement = document.querySelector('.films');
 if (FILMS_COUNT > 0) {
   render(filmsMainElement, createFilmsListTemplate('All movies. Upcoming', true), 'beforeend');
   const filmsListElement = filmsMainElement.querySelector('.films-list');
-  if (FILMS_COUNT > 5) {
+  if (FILMS_COUNT > INIT_FILMS_COUNT) {
     render(filmsListElement, createShowMoreButtonTemplate(), 'beforeend'); // Создание кнопки показать далее
   }
 } else {
@@ -49,7 +46,7 @@ if (FILMS_COUNT > 0) {
 const filmsListContainer = filmsMainElement.querySelectorAll('.films-list__container')[0];
 
 let indexFilm = 0;
-if (filmsListContainer) {
+if (FILMS_COUNT > 0) {
   for (let i = indexFilm; i < INIT_FILMS_COUNT; i++) { // Наполнение блока фильмами
     render(filmsListContainer, createFilmCardTemplate(films[i]), 'beforeend');
     indexFilm = i;
@@ -60,14 +57,19 @@ render(siteMainElement, createFooterTemplate(countMoviesInside), 'afterend');
 
 const showMoreButton = filmsMainElement.querySelector('.films-list__show-more');
 
-showMoreButton.addEventListener('click', () => { // Клик по кнопке Показать далее
-  for (let i = 0, j = indexFilm + 1; i < SHOW_MORE_COUNT; i++, j++) {
-    if (j === FILMS_COUNT) {
-      const buttonShowMore = filmsMainElement.querySelector('.films-list__show-more');
-      buttonShowMore.remove();
-      break;
+if (FILMS_COUNT > INIT_FILMS_COUNT) {
+  showMoreButton.addEventListener('click', () => { // Клик по кнопке Показать далее
+    for (let i = 0, j = indexFilm + 1; i < SHOW_MORE_COUNT; i++, j++) {
+      if (j === FILMS_COUNT - 1) {
+        const buttonShowMore = filmsMainElement.querySelector('.films-list__show-more');
+        buttonShowMore.remove();
+      }
+      if (j === FILMS_COUNT) {
+        break;
+      }
+      render(filmsListContainer, createFilmCardTemplate(films[j]), 'beforeend');
+      indexFilm = j;
     }
-    render(filmsListContainer, createFilmCardTemplate(films[j]), 'beforeend');
-    indexFilm = j;
-  }
-});
+  });
+}
+
--- src/mock/other.js
+++
@@ -1,5 +0,0 @@
-export const countWatchlist = 11;
-export const countHistory = 3;
-export const countFavorites = 7;
-export const countMoviesInside = 130291;
-export const countViewedFilms = 28;
